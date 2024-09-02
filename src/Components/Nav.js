4242fce4c278dd897749931767da158e21cced8b
Nikhil-06-06 <nikhilraw2@gmail.com>
2024-09-02 16:26:40 +0530

    Added app logo

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import { appTitle } from '../Constants/Constants'
+import React from "react";
+import { appTitle } from "../Constants/Constants";
+import { NotebookPen } from "lucide-react";
 
 function Nav() {
   return (
-    <div className='nav'>{appTitle}</div>
-  )
+    <div className="nav">
+      <NotebookPen />
+      &nbsp;
+      <span>{appTitle}</span>
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
